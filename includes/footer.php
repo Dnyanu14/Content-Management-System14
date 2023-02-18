f2b8935b6a3245d16aaa4f3056b457dadb048ccc
Dnyanu14 <fundednyaneshwari1440@gmail.com>
2023-02-18 14:59:02 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+
+        <!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; Your Website 2021.</p>
+                    <h5><b>Made By Kunal Pardeshi(KP).</b></h5>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+
---
+++ includes/sidebar.php
@@ -0,0 +1,75 @@
+       <div class="col-md-4">
+
+                <!-- Blog Search Well -->
+                <div class="well">
+                    <h4>Blog Search</h4>
+                    <form action="search.php" method="post">
+                    <div class="input-group">
+                        <input type="text" name="search" class="form-control">
+                        <span class="input-group-btn">
+                            <button class="btn btn-default" name="submit" type="submit">
+                                <span class="glyphicon glyphicon-search"></span>
+                        </button>
+                        </span>
+                    </div>
+                    </form>
+                    <!-- /.input-group -->
+                </div>
+                
+                <!-- Login Form -->
+                <div class="well">
+                    <h4>Login</h4>
+                    <form action="includes/login.php" method="post">
+                    <div class="form-group">
+                        <input type="text" name="username" class="form-control" placeholder="Enter Username">
+                        <input type="password" name="password" class="form-control" placeholder="Enter Password">
+                    </div>
+                    <div class="input-group">
+                        <span class="input-group-btn">
+                        <button class="btn btn-primary" name="login" type="submit">Login</button>
+                        </span>
+                    </div>
+                    </form>
+                    <!-- /.input-group -->
+                </div>
+
+                <!-- Blog Categories Well -->
+                <div class="well">
+                   <?php
+                    $qry = "SELECT * FROM categories";
+                    $result=mysqli_query($connection,$qry);
+                    ?>
+                    <h4>Blog Categories</h4>
+                    <div class="row">
+                        <div class="col-lg-6">
+                            <ul class="list-unstyled">
+                            <?php
+                            while($row = mysqli_fetch_assoc($result))
+                            {
+                                $cat_title = $row['cat_title'];
+                                echo "<li><a href='#'>{$cat_title}</a>
+                                </li>";
+                            }
+                            ?>
+                             <!--   <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>
+                                <li><a href="#">Category Name</a>
+                                </li>   -->
+                             </ul>
+                        </div>
+                        ->
+                    </div>
+                    <!-- /.row -->
+                </div>
+
+                <!-- Side Widget Well -->
+                <!--<div class="well">
+                    <h4>Side Widget Well</h4>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore, perspiciatis adipisci accusamus laudantium odit aliquam repellat tempore quos aspernatur vero.</p>
+                </div>-->
+
+            </div>
